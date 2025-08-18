34e9f4ba0631a7490b52cb8dba819a6e0c598854
AdhityaMaulana11 <adhityamaulanawijaya11@gmail.com>
2025-08-18 18:57:46 +0700

    refactor: AboutTabsSection convert to tailwind

--- src/components/about/AboutTabsSection.jsx
+++ src/components/about/AboutTabsSection.jsx
@@ -3,14 +3,15 @@ import TabButton from "./TabButton";
 import aboutTabsData from "../../data/aboutTabsData";
 import AboutContentSection from "./AboutContentSection";
 
-function AboutTabsSection() {
+export default function AboutTabsSection() {
   const [activeTab, setActiveTab] = useState(aboutTabsData[0].key);
-  const activeData = aboutTabsData.find(tab => tab.key === activeTab);
+  const activeData = aboutTabsData.find((tab) => tab.key === activeTab);
 
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '1rem', margin: '1.5rem 0' }}>
-        {aboutTabsData.map(tab => (
+      {/* Tabs */}
+      <div className="flex justify-center gap-4 my-6">
+        {aboutTabsData.map((tab) => (
           <TabButton
             key={tab.key}
             active={activeTab === tab.key}
@@ -20,9 +21,12 @@ function AboutTabsSection() {
           </TabButton>
         ))}
       </div>
-      <hr style={{ border: "none", borderBottom: "2px solid #e0e0e0", margin: "0 auto 1.5rem auto", width: "90%" }} />
+
+      {/* Divider */}
+      <hr className="border-0 border-b-2 border-gray-300 w-[90%] mx-auto mb-6" />
+
+      {/* Content */}
       <AboutContentSection data={activeData} />
     </>
   );
 }
-export default AboutTabsSection;
