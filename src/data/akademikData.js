230738dfc8ca20d9af9a934ce4517524b066872f
AdhityaMaulana11 <adhityamaulanawijaya11@gmail.com>
2025-08-18 13:31:47 +0700

    chore(akademik data): tambah data detail prodi FEB

--- src/data/akademikData.js
+++ src/data/akademikData.js
@@ -53,7 +53,42 @@ Secara garis besar materi perkuliahan pada program studi ini disesuaikan dengan 
   {
     nama: "Fakultas Ekonomi & Bisnis (FEB)",
     prodi: [
-      { jenjang: "S1", nama: "Manajemen", akreditasi: "Baik Sekali" },
+      {
+        jenjang: "S1",
+        nama: "Manajemen",
+        akreditasi: "Baik Sekali",
+        detail: {
+          deskripsi: `Di pebisnis yang sukses pasti ada kendala yang harus dilalui sebelumnya. Dengan mengikuti proses pembelajaran di Prodi Manajemen Universitas Catur Insan Cendekia, mahasiswa dilatih untuk selalu mencari solusi inovatif dalam menanggapi setiap tantangan bisnis yang dihadapi.
+
+Proses pembelajaran di Prodi Manajemen dirancang untuk mempersiapkan mahasiswa dengan kompetisi global ke depan. Melalui pendekatan blended learning, mahasiswa menjalani pembelajaran dan mencerminkan pembelajaran mereka ke dalam konseptualisasi. Pendekatan ini menuntun siswa untuk memecahkan akar masalah melalui kerangka kerja pembelajaran berbasis project. Mahasiswa Program Bisnis S1 juga diundang ke berbagai kegiatan belajar yang meningkatkan keterampilan, pengetahuan, dan sikap mereka. mahasiswa akan mengalami studi kasus, perusahaan, dan kunjungan awal, perencanaan bisnis, simulasi bisnis, dan project kewirausahaan lainnya yang bertujuan untuk menghasilkan kreativitas dan inovasi siswa. Proses pembelajaran Program Manajemen membiasakan mahasiswa dengan suasana kerja dan bisnis. Situasi ini akan mensimulasikan sensitivitas proses bisnis mahasiswa dan memberikan mereka keterampilan pengambilan keputusan yang bijaksana.`,
+          visi: `Menjadi Program Studi Manajemen yang berorientasi dalam bidang kewirausahaan dan teknologi untuk mendukung masyarakat daerah dengan menghasilkan lulusan di bidang manajemen dan bisnis yang mampu untuk menanggapi perubahan zaman`,
+          misi: [
+            "Menyelenggarakan pengajaran di bidang ilmu manajemen yang berfokus untuk menghasilkan lulusan-lulusan dalam bidang manajemen dan bisnis yang mampu untuk menanggapi perubahan zaman.",
+            "Mendukung dan menciptakan lingkungan yang positif untuk pembelajaran, inovasi dan penerapan dalam bidang ilmu manajemen dan bisnis yang bersinergi dengan kewirausahaan dan teknologi informasi.",
+            "Menjalankan aktivitas tridharma di bidang Manajemen dan bisnis untuk memberikan kontribusi terhadap masyarakat daerah.",
+            "Menyelenggarakan pendidikan yang terjangkau dalam memperluas akses pendidikan di bidang manajemen dan bisnis pada masyarakat daerah.",
+          ],
+          prospek: [
+            "Entrepreneur",
+            "Professional in Business Development Area",
+            "Business Planning",
+            "Business Analyst",
+            "Management & Business Consultant",
+            "Small Business Manager",
+            "Business Advisor",
+            "Business Researcher",
+            "Business Economist",
+            "General Management",
+            "Administrative Manager",
+            "Executive Manager",
+            "Project Manager",
+            "Trade Manager",
+            "CEO/COO",
+            "etc.",
+          ],
+          kurikulum: "Kurikulum Manajemen S1",
+        },
+      },
       { jenjang: "S1", nama: "Akuntansi", akreditasi: "Baik" },
       {
         jenjang: "S1",
