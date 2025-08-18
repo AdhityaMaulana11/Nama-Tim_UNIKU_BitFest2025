f3ef6aa3aef2b2b77b3376ad088e020e1f79068c
AdhityaMaulana11 <adhityamaulanawijaya11@gmail.com>
2025-08-18 13:24:33 +0700

    chore(akademik data): tambah data detail prodi Teknik Informatika

--- src/data/akademikData.js
+++ src/data/akademikData.js
@@ -2,7 +2,40 @@ export const akademikData = [
   {
     nama: "Fakultas Teknologi & Informasi (FTI)",
     prodi: [
-      { jenjang: "S1", nama: "Teknik Informatika", akreditasi: "B" },
+      {
+        jenjang: "S1",
+        nama: "Teknik Informatika",
+        akreditasi: "B",
+        detail: {
+          deskripsi: `Program Studi Teknik Informatika memberikan penguasaan dan kemampuan kepada mahasiswa untuk membangun dan mengembangkan berbagai aspek teknis teknologi informasi. Penekanan dari program studi ini adalah pada bidang pemrograman dan komputasi, rekayasa perangkat lunak untuk berbagai bidang aplikasi dalam berbagai bidang usaha, dan teknologi jaringan komputer.
+
+Selain itu program studi ini memberikan bidang-bidang ilmu yang berkaitan dengan pengembangan sistem informasi dan pemanfaatan teknologi lainnya dengan tujuan dapat meningkatkan efisiensi kerja di berbagai bidang usaha yang menerapkan komputerisasi.
+
+Program Studi Teknik Informatika akan memberikan bekal kepada mahasiswa untuk mampu melakukan analisa dan perancangan komputasi, melakukan rekayasa perangkat lunak untuk meningkatkan performance perangkat lunak, dan penguasaan dalam teknik-teknik optimasi perangkat lunak.
+
+Secara garis besar materi perkuliahan pada program studi ini disesuaikan dengan kurikulum nasional dan mengacu kepada kebutuhan pasar dunia kerja yang dapat dikelompokkan menjadi :
+1. Pemrograman dan Komputasi
+2. Rekayasa Perangkat Lunak (Software Engineering)
+3. Arsitektur dan Jaringan Komputer`,
+          visi: `Menjadi Program Studi unggulan dalam bidang informatika khususnya bidang rekayasa perangkat lunak dan jaringan di wilayah Ciayumajakuning (Cirebon, Indramayu, Majalengka, dan Kuningan)`,
+          misi: [
+            "Membangun insan cendekia yang unggul dengan penyelenggaraan pendidikan berkualitas dan kondusif yang berkelanjutan dengan penerapan kurikulum yang selaras dengan kebutuhan saat ini dan yang akan datang melalui metode pembelajaran problem based learning.",
+            "Meningkatkan bidang keahlian dan kompetensi serta membangun jiwa kewirausahaan dengan memanfaatkan teknologi informasi dalam penerapannya diberbagai bidang.",
+            "Meningkatkan budaya penelitian untuk mengembangkan keilmuan bidang informatika, khususnya dalam bidang rekayasa perangkat lunak dan jaringan.",
+            "Membagikan hasil pengembangan keilmuan kepada masyarakat melalui kegiatan pengabdian kepada masyarakat.",
+          ],
+          prospek: [
+            "System Analyst",
+            "System Engineer/Programmer",
+            "Web Developer/Programmer",
+            "Software Engineer",
+            "Application Programmer",
+            "Data Base Specialist",
+            "Data Base Administrator",
+          ],
+          kurikulum: "Kurikulum Teknik Informatika S1",
+        },
+      },
       { jenjang: "S1", nama: "Sistem Informasi", akreditasi: "Baik Sekali" },
       { jenjang: "S1", nama: "Desain Komunikasi Visual", akreditasi: "Baik" },
       {
