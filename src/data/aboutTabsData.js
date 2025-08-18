2bcde4b47ec6f4889c7b305a40b6722c5b81d749
AdhityaMaulana11 <adhityamaulanawijaya11@gmail.com>
2025-08-18 23:27:12 +0700

    fix: images di about page sejarah section

--- src/data/aboutTabsData.js
+++ src/data/aboutTabsData.js
@@ -5,7 +5,7 @@ const aboutTabsData = [
     title: "Sambutan Rektor",
     plainText: "",
     containerText: `Assalamu'alaikum Wr.Wb.,\nSalam Sejahtera,\nPuji syukur ke hadirat Tuhan Yang Maha Esa, yang tidak henti-hentinya mencurahkan Rahmat dan HidayahNya bagi kita semua.\nKehadiran website Universitas CIC yang mudah diakses, akurat dan informatif, kiranya dapat mempermudah bagi keluarga besar civitas akademika Universitas CIC dan masyarakat luas untuk mendapatkan informasi tentang Universitas CIC, pendaftaran, Beasiswa, Sistem Informasi Manajemen Akademik (SIMAK), perpustakaan, kegiatan Olahraga, Iptek, Seni Budaya, Penelitian dan informasi lainnya.\nPendirian Universitas CIC diawali dengan berdirinya Lembaga Pendidikan Kursus dan Pelatihan CIC (dahulu Cirebon Institute of Computer) pada tanggal 13 Januari 1984, yang berarti sudah lebih dari 30 tahun secara konsisten dan komitmen berkiprah di dunia pendidikan. Pada tahun 1999 Lembaga Pendidikan CIC mengembangkan diri dengan mendirikan Sekolah Tinggi Manajemen Informatika dan Komputer Catur Insan Cendekia (STMIK CIC) dengan ijin operasional resmi dari Menteri Pendidikan Nasional nomer 123/D/O/1999 dan pada tanggal 21 Oktober 2019 STMIK CIC beralih status menjadi Universitas CIC yang telah disahkan oleh kemenristekdik.\nKonsistensi Universitas CIC sebagai IT (Information Technology) SCHOOL diharapkan mampu memberikan kontribusi positif berupa ilmu pengetahuan berbasis IT kepada dunia usaha dan industri serta mendukung langkah-langkah pemerintah daerah se-karesidenan Cirebon menuju Good Corporate Governance\nUniversitas CIC sejak tahun 2005 telah mendapatkan akreditasi dari BAN-PT(Badan Akreditasi Perguruan Tinggi) untuk 4 (empat) program studi yang ada di lingkungan STMIK CIC,yaitu Program Studi Sistem Informasi, Program Studi Teknik Informatika, Program Studi Manajemen Informatika dan Program Studi Komputerisasi Akuntansi, sehingga STMIK CIC menjadi satu-satunya Perguruan Tinggi Komputer yang semua program studinya terakreditasi oleh BAN-PT dan proaktif untuk terus melakukan re-akreditasi pada setiap program studi yang ada di Universitas CIC.\nUniversitas CIC juga menjalankan perannya sebagai Knowledge based dengan melakukan kerjasama-kerjasama dengan dunia usaha dan industri serta pemerintah daerah se-karesidenan Cirebon, baik berupa kajian kurikulum berbasis PBL (Problem Based Learning), pengiriman tenaga praktisi untuk mengajar di Universitas CIC maupun penyaluran mahasiswa/i untuk praktek kerja lapang dan menyerap lulusan Universitas CIC serta menjadikan lulusan Universitas CIC sebagai wirausaha tangguh di era global.\nInsya Allah Universitas CIC mampu menjadi institusi pendidikan tinggi di Cirebon yang mampu menghasilkan sumber daya manusia yang siap bersaing dipasar global sesuai dengan Visi dan Misi Universitas CIC. Kami mohon dukungan dari semua pihak, terima kasih.\nWassalamu'alaikum Wr.Wb.,\nRektor Universitas CIC\nAssoc. Prof. Dr. Chandra Lukita, S.E., M.M., M.TI.`,
-    images: []
+    images: [],
   },
   {
     key: "visi-misi",
@@ -17,7 +17,7 @@ const aboutTabsData = [
       "Menyelenggarakan pengajaran berfokus pada menghasilkan lulusan yang mampu menanggapi perubahan zaman.",
       "Mendukung dan menciptakan lingkungan positif untuk pembelajaran, inovasi, dan penerapan teknologi dan kewirausahaan.",
       "Menjalankan aktivitas Tri Dharma untuk memberikan kontribusi terhadap masyarakat daerah.",
-      "Menyelenggarakan pendidikan tinggi yang terjangkau untuk memperluas akses pendidikan masyarakat daerah."
+      "Menyelenggarakan pendidikan tinggi yang terjangkau untuk memperluas akses pendidikan masyarakat daerah.",
     ],
     tujuan: [
       "Menghasilkan mahasiswa yang unggul dan berprestasi di tingkat nasional serta memiliki jiwa technopreneur.",
@@ -27,21 +27,22 @@ const aboutTabsData = [
       "Menciptakan suasana akademik yang kondusif bagi pengembangan penelitian di institusi.",
       "Memberikan kontribusi positif bagi masyarakat melalui kegiatan pengabdian kepada masyarakat yang tepat dan bermanfaat.",
       "Mewujudkan tata pamong dan tata kelola organisasi yang efektif dan efisien.",
-      "Menghasilkan kerjasama yang produktif dan bermanfaat bagi institusi dan industri/UMKM."
+      "Menghasilkan kerjasama yang produktif dan bermanfaat bagi institusi dan industri/UMKM.",
     ],
-    images: []
+    images: [],
   },
   {
     key: "sejarah",
     label: "Sejarah UCIC",
     title: "Sejarah UCIC",
-    plainText: "CIC mulai didirikan pada tahun 1984 sebagai lembaga pendidikan kursus dan pelatihan. Pada tahun 1994, CIC mendirikan Akademi Perdagangan Widya Dharma. Kemudian Lembaga pendidikan CIC berkembang dengan mendirikan Sekolah Tinggi Manajemen Informatika dan Komputer (STMIK) Catur Insan Cendekia pada 1999. Pada tahun 2006 Akademi Perdagangan Widya Dharma berubah nama menjadi Akademi Perdagangan CIC. Universitas Catur Insan Cendekia (UCIC) merupakan hasil penggabungan antara Akademi Perdagangan Catur Insan Cendekia dan Sekolah Tinggi Manajemen Informatika Komputer (STMIK) CIC pada tahun 2019, dan mendapat pengesahan dengan terbitnya Surat Keputusan Menteri Riset, Teknologi dan Pendidikan Tinggi Republik Indonesia Nomor 952/KPT/I/2019 tertanggal 10 Oktober 2019. UCIC saat ini memiliki 2 Fakultas, yaitu Fakultas Ekonomi dan Bisnis serta Fakultas Teknologi Informasi. \n\nUniversitas Catur Insan Cendekia (UCIC) sebagai holding bagi unit bisnis dan usaha yang ada di bawahnya. UCIC saat ini memiliki ekosistem di bidang pendidikan, kemanusiaan atau sosial, dan Unit bisnis yang mendukung pelaksanaan kegiatan Tridharma Perguruan Tinggi serta menjadi other income bagi UCIC. Di bidang pendidikan, terdapat Sekolah Pelita Bangsa Cambridge International School dari jenjang Playgroup hingga SMA dengan status akreditasi A pada setiap jenjang yang didirikan pada tahun 2006, International Language school EF English First Kota Cirebon yang didirikan pada tahun 1997, Enspire School of Digital Art (ESDA) pada 2019, dan Code First yang didirikan pada 2020. Di bidang Sosial dan Kemasyarakatan terdapat Harapanku Insan Sejahtera yang didirikan pada tahun 2001, yang membantu masyarakat disabilitas, dan mendampingi serta melatih kemandirian anak-anak Down Syndrome melalui Griya Karya Harapanku. Di bidang unit bisnis terdapat Bisnis Properti berupa Perumahan Taman Cipto yang didirikan pada 2006 dan Perumahan Taman Wahidin yang didirikan pada 1996. Bisnis lainnya adalah Sport Center Taman Cipto, Sport Centre Taman Wahidin, Retail Balmart dan Startup Code First.",
+    plainText:
+      "CIC mulai didirikan pada tahun 1984 sebagai lembaga pendidikan kursus dan pelatihan. Pada tahun 1994, CIC mendirikan Akademi Perdagangan Widya Dharma. Kemudian Lembaga pendidikan CIC berkembang dengan mendirikan Sekolah Tinggi Manajemen Informatika dan Komputer (STMIK) Catur Insan Cendekia pada 1999. Pada tahun 2006 Akademi Perdagangan Widya Dharma berubah nama menjadi Akademi Perdagangan CIC. Universitas Catur Insan Cendekia (UCIC) merupakan hasil penggabungan antara Akademi Perdagangan Catur Insan Cendekia dan Sekolah Tinggi Manajemen Informatika Komputer (STMIK) CIC pada tahun 2019, dan mendapat pengesahan dengan terbitnya Surat Keputusan Menteri Riset, Teknologi dan Pendidikan Tinggi Republik Indonesia Nomor 952/KPT/I/2019 tertanggal 10 Oktober 2019. UCIC saat ini memiliki 2 Fakultas, yaitu Fakultas Ekonomi dan Bisnis serta Fakultas Teknologi Informasi. \n\nUniversitas Catur Insan Cendekia (UCIC) sebagai holding bagi unit bisnis dan usaha yang ada di bawahnya. UCIC saat ini memiliki ekosistem di bidang pendidikan, kemanusiaan atau sosial, dan Unit bisnis yang mendukung pelaksanaan kegiatan Tridharma Perguruan Tinggi serta menjadi other income bagi UCIC. Di bidang pendidikan, terdapat Sekolah Pelita Bangsa Cambridge International School dari jenjang Playgroup hingga SMA dengan status akreditasi A pada setiap jenjang yang didirikan pada tahun 2006, International Language school EF English First Kota Cirebon yang didirikan pada tahun 1997, Enspire School of Digital Art (ESDA) pada 2019, dan Code First yang didirikan pada 2020. Di bidang Sosial dan Kemasyarakatan terdapat Harapanku Insan Sejahtera yang didirikan pada tahun 2001, yang membantu masyarakat disabilitas, dan mendampingi serta melatih kemandirian anak-anak Down Syndrome melalui Griya Karya Harapanku. Di bidang unit bisnis terdapat Bisnis Properti berupa Perumahan Taman Cipto yang didirikan pada 2006 dan Perumahan Taman Wahidin yang didirikan pada 1996. Bisnis lainnya adalah Sport Center Taman Cipto, Sport Centre Taman Wahidin, Retail Balmart dan Startup Code First.",
     containerText: "",
     images: [
-      "../assets/images/milestonecic-1.png",
-      "../assets/images/milestonecic.png"
-    ]
-  }
+      "src/assets/images/ekosistemcic-1.png",
+      "src/assets/images/milestonecic-1.png",
+    ],
+  },
 ];
 
 export default aboutTabsData;
