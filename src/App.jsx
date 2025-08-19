ce550c77a6318adf3dbb5529cac0ca05d9da97f6
timbubadibako <pjrlywm@gmail.com>
2025-08-19 13:04:01 +0700

    Menambahkan Warta di landing page, setting scroll-top behavior di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,21 @@ import AboutPage from "./views/AboutPage";
 import NewsPage from "./views/NewsPage";
 import NewsDetailPage from "./views/NewsDetailPage";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
